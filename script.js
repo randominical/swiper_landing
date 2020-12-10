5bfa3b7f94aafd8133e2be00cb0bd0b8ee2b2a19
randominical <aa.agafonova@gmail.com>
2020-12-10 19:42:16 +0300

    Настройки прокрутки 1

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 let pageSlider = new Swiper('.page', {
+    //переназначение стандартных классов
+    wrapperClass: "page__wrapper",
+    slideClass: "page__screen",
+
+    //вертикальный слайдер
+    direction: 'vertical',
+
+    //количество слайдов для показа
+    slidePerView: 'auto',
+
+    //включаем параллакс
+    parallax: true,
     
+    //управление клавиатурой
+    keyboard: {
+        //включить/выключить
+        enabled: true,
+        //включить/выключить только когда слайдер в пределах вьюпорта
+        onlyInViewport: true,
+        //включить/выключить управление клавишами pageUp, pageDown
+        pageUpDown: true,
+    },
+    
+    //управление колесом мыши
+    mousewheel: {
+        //чувствительность колеса мыши
+        sensitivity: 1,
+        //класс объекта, на котором будет срабатывать прокрутка мышью
+        //eventsTarget: '.image-slider'
+    },
 })
